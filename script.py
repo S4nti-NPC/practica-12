d7e529d86178648d1af453c306e2acebf093ea9d
Santiago <Al03103635@tecmilenio.mx>
2025-04-21 13:35:59 -0600

    Error provocado

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ print("🔧 Ejecutando script de prueba...")
 
 try:
     # Generar archivo de resultados
-    with open("resultado.txt", "w") as file:
+    wh open("resultado.txt", "w") as file:
         file.write(f"Fecha de ejecución: {datetime.datetime.now()}\n")
         file.write("Estado: ÉXITO\n")
     print("✅ Archivo 'resultado.txt' creado!")
